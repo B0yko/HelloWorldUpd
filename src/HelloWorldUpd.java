948a193e2817cf04e3f1c91d1e87e7ae713bd448
andrejbojko <andriy.boyko@auk.edu.ua>
2023-08-22 16:03:29 +0300

    0

--- src/HelloWorldUpd.java
+++ src/HelloWorldUpd.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class HelloWorldUpd {
+public class HelloWorldUpd {
+    static public void main(String[]args) {
+        System.out.println("Hello World!");
+        System.out.println("My name is Andriy. Welcome to my first Java program");
+    }
 }
